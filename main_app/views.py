c63f1346497b21ac048c8928a1ada25574c5fd18
Benita <bmwint22@gmail.com>
2025-03-14 15:44:23 -0500

    project progress

--- main_app/views.py
+++ main_app/views.py
@@ -4,19 +4,30 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>Hello Designer Bags!</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
+def bag_index(request):
+    return render(request, 'bags/index.html', {'bags': bags})
+
 
 class Bag:
-    def __init__(brand, name, color, texture, description):
+    def __init__(self, brand, name, color, texture, description):
         self.brand = brand
         self.name = name
         self.color = color
         self.texture = texture
         self.description = description
+        
+bags = [
+    Bag('Dior', 'Mini Lady', 'Cherry Red', 'Patent Cannage Calfskin', 'Epitomizes elegance & beauty'),
+    Bag('Chanel', 'Small Flap Bag', 'Light Yellow', 'Shiny Lambskin', 'Spring & Summer 2025 pre-collection'),
+    Bag('Gucci', 'Small GG Marmont Shoulder Bag', 'Black Matelassé', 'Chevron Leather', 'Adjustable chain shoulder strap'),
+    Bag('YSL', 'Calypso Small', 'Vert Emeraude', 'Jacquard', 'Supple bag in baroque floral jacquard'),
+    
+]
         
 
         
