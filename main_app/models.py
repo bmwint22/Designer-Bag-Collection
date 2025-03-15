f5c6fe99b54353e892f6e6df4a47d54ef698e198
Benita <bmwint22@gmail.com>
2025-03-15 15:12:34 -0500

    progress

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Bag(models.Model):
+    brand = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+    texture = models.CharField(max_length=20)
+    description = models.TextField(max_length=50)
+    
+    def __str__(self):
+        return self.brand
+    
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Bag
+
 
 def home(request):
     return render(request, 'home.html')
@@ -10,25 +12,14 @@ def about(request):
     return render(request, 'about.html')
 
 def bag_index(request):
+    bags = Bag.objects.all()
     return render(request, 'bags/index.html', {'bags': bags})
 
 
-class Bag:
-    def __init__(self, brand, name, color, texture, description):
-        self.brand = brand
-        self.name = name
-        self.color = color
-        self.texture = texture
-        self.description = description
-        
-bags = [
-    Bag('Dior', 'Mini Lady', 'Cherry Red', 'Patent Cannage Calfskin', 'Epitomizes elegance & beauty'),
-    Bag('Chanel', 'Small Flap Bag', 'Light Yellow', 'Shiny Lambskin', 'Spring & Summer 2025 pre-collection'),
-    Bag('Gucci', 'Small GG Marmont Shoulder Bag', 'Black Matelassé', 'Chevron Leather', 'Adjustable chain shoulder strap'),
-    Bag('YSL', 'Calypso Small', 'Vert Emeraude', 'Jacquard', 'Supple bag in baroque floral jacquard'),
-    
-]
-        
 
-        
+
+try:
+    bag = Bag.objects.get(id=1)
+except Bag.DoesNotExist:
+    print("This bag does not exist!")
 
