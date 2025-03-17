0a6bbde07ad2946cd81b921f73907495dce2db5b
Benita <bmwint22@gmail.com>
2025-03-17 11:54:58 -0500

    progress

--- main_app/views.py
+++ main_app/views.py
@@ -3,8 +3,6 @@ from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Bag
 
-
-
 def home(request):
     return render(request, 'home.html')
 
@@ -14,29 +12,24 @@ def about(request):
 def bag_index(request):
     bags = Bag.objects.all()
     return render(request, 'bags/index.html', {'bags': bags})
-try:
-    bag = Bag.objects.get(id=1)
-except Bag.DoesNotExist:
-    print("This bag does not exist!")
-
 
 def bag_detail(request, bag_id):
-    Bag = Bag.objects.get(id=bag_id)
-    return render(request, 'bags/detail.html', {'bag': bag})
-
-
+    try:
+        bag = Bag.objects.get(id=bag_id)
+    except Bag.DoesNotExist:
+        return HttpResponse("This bag does not exist!")  
     
+    return render(request, 'bags/detail.html', {'bag': bag})
 class BagCreate(CreateView):
     model = Bag
     fields = '__all__'
     success_url = '/bags/'
-    
+
 class BagUpdate(UpdateView):
     model = Bag
     fields = ['name', 'color', 'texture', 'description']
-    
+    success_url = '/bags/'  
+
 class BagDelete(DeleteView):
     model = Bag
     success_url = '/bags/'
-    
-    
